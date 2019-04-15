5091d635b6648982b95c47fba620f2ee14c32162
Nishit Mehta <nishit.mehta1@gmail.com>
2019-04-14 23:36:06 -0500

    Updates

---
+++ precache-manifest.8acde631d75ec40f7208e897a3fdeadc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56dafe5553b353d443fb",
+    "url": "/reactjs-simpleCounter/static/js/runtime~main.71d7c37e.js"
+  },
+  {
+    "revision": "7eb9528bff08d910209b",
+    "url": "/reactjs-simpleCounter/static/js/main.4d1ccaa4.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/reactjs-simpleCounter/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7eb9528bff08d910209b",
+    "url": "/reactjs-simpleCounter/static/css/main.943ba3d4.chunk.css"
+  },
+  {
+    "revision": "c93e6ae303f100aceca6f711764a1228",
+    "url": "/reactjs-simpleCounter/index.html"
+  }
+];
